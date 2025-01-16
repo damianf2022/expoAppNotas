3e64e5dbb5a0e14224960b2093761b00f4cef779
Mauro Sergio <damianf2022@gmail.com>
2025-01-16 16:38:51 -0300

    Configuración del proyecto Expo React Native con navegación básica

--- expoAppNotas/App.js
+++ expoAppNotas/App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+// Only import react-native-gesture-handler on native platforms
+import "react-native-gesture-handler";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from '@react-navigation/native';
+
+import CreateNote from "./screen/CreateNote";
+import DetailsNotes from "./screen/DetailsNotes";
+import Home from "./screen/Home";
 
 export default function App() {
+  const Stack = createStackNavigator();
+
+  function MyStack() {
+    return (
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="CreateNotes" component={CreateNote} />
+        <Stack.Screen name="DetailsNotes" component={DetailsNotes} />
+      </Stack.Navigator>
+    );
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ expoAppNotas/screen/DetailsNotes.jsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function DetailsNotes() {
+  return (
+    <View>
+      <Text>Details</Text>
+    </View>
+  );
+}
